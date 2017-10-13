7e6484bd0b63436ede25c0b87df2182e0a4548f8
puffy <nagy.gergo@3pixel.hu>
2017-10-13 23:38:43 +0200

    init commit with basic idp and the quickstart ui

---
+++ src/Marvin.IDP/Config.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace Marvin.IDP
+{
+    public static class Config
+    {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "12345678-12345678-12345678-12345678",
+                    Username = "Frank",
+                    Password = "pass",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Frank"),
+                        new Claim("family_name", "Underwood")
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "87654321-87654321-87654321-87654321",
+                    Username = "Claire",
+                    Password = "pass",
+
+                    Claims = new List<Claim>
+                    {
+                        new Claim("given_name", "Claire"),
+                        new Claim("family_name", "Underwood")
+                    }
+                }
+            };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>();
+        }
+    }
+}
